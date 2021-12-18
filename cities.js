e3d1eafbb6de15fa5f985255e69f170dfbe822c5
sheikh-zafar <audios.shkzafar@gmail.com>
2021-12-18 07:26:24 +0530

    commit

---
+++ cities.js
@@ -0,0 +1,292 @@
+export const cities = [
+  {
+    id: 1,
+    city: "Abu al Bukhoosh",
+  },
+  {
+    id: 2,
+    city: "Abu Dhabi",
+  },
+  {
+    id: 3,
+    city: "Abu Musa",
+  },
+  {
+    id: 4,
+    city: "Ahmed Bin Rashid Port",
+  },
+  {
+    id: 5,
+    city: "Ajman",
+  },
+  {
+    id: 6,
+    city: "Al Ain",
+  },
+  {
+    id: 7,
+    city: "Al Barsha",
+  },
+  {
+    id: 8,
+    city: "Al Dhafra",
+  },
+  {
+    id: 9,
+    city: "Al Fujayrah",
+  },
+  {
+    id: 10,
+    city: "Al Hamriyah",
+  },
+  {
+    id: 11,
+    city: "Al Jazeera Port",
+  },
+  {
+    id: 12,
+    city: "Al Jeer Port",
+  },
+  {
+    id: 13,
+    city: "Al Mafraq",
+  },
+  {
+    id: 14,
+    city: "Al Quoz",
+  },
+  {
+    id: 15,
+    city: "Al Sufouh",
+  },
+  {
+    id: 16,
+    city: "Ar Ruways",
+  },
+  {
+    id: 17,
+    city: "Arzanah Island",
+  },
+  {
+    id: 18,
+    city: "Das Island",
+  },
+  {
+    id: 19,
+    city: "Deira",
+  },
+  {
+    id: 20,
+    city: "Dibba",
+  },
+  {
+    id: 21,
+    city: "Dubai",
+  },
+  {
+    id: 22,
+    city: "Dubai World Central Apt",
+  },
+  {
+    id: 23,
+    city: "Esnnad",
+  },
+  {
+    id: 24,
+    city: "Fateh Terminal",
+  },
+  {
+    id: 25,
+    city: "Free Port",
+  },
+  {
+    id: 26,
+    city: "Habshan",
+  },
+  {
+    id: 27,
+    city: "Hail",
+  },
+  {
+    id: 28,
+    city: "Hamriya Free Zone Port",
+  },
+  {
+    id: 29,
+    city: "Hassyan",
+  },
+  {
+    id: 30,
+    city: "Hatta",
+  },
+  {
+    id: 31,
+    city: "Hulaylah Terminal",
+  },
+  {
+    id: 32,
+    city: "Indooroodilly",
+  },
+  {
+    id: 33,
+    city: "Jebel Ali",
+  },
+  {
+    id: 34,
+    city: "Jebel Ali Free Zone",
+  },
+  {
+    id: 35,
+    city: "Jebel Dhanna",
+  },
+  {
+    id: 36,
+    city: "Jumayrah",
+  },
+  {
+    id: 37,
+    city: "Kalba",
+  },
+  {
+    id: 38,
+    city: "Khalidia",
+  },
+  {
+    id: 39,
+    city: "Khor al Fakkan",
+  },
+  {
+    id: 40,
+    city: "Masfut",
+  },
+  {
+    id: 41,
+    city: "Mina Khalid",
+  },
+  {
+    id: 42,
+    city: "Mina Khalifa/Abu Dhabi",
+  },
+  {
+    id: 43,
+    city: "Mina Rashid Port",
+  },
+  {
+    id: 44,
+    city: "Mina Saqr",
+  },
+  {
+    id: 45,
+    city: "Mina Zayed/Abu Dhabi",
+  },
+  {
+    id: 46,
+    city: "Minhad",
+  },
+  {
+    id: 47,
+    city: "Mirfa",
+  },
+  {
+    id: 48,
+    city: "Mubarek Terminal",
+  },
+  {
+    id: 49,
+    city: "Mubarras Island",
+  },
+  {
+    id: 50,
+    city: "Musafa",
+  },
+  {
+    id: 51,
+    city: "Mussafah",
+  },
+  {
+    id: 52,
+    city: "Offshore Fujairah",
+  },
+  {
+    id: 53,
+    city: "Port Rashid",
+  },
+  {
+    id: 54,
+    city: "Rak Khor Port",
+  },
+  {
+    id: 55,
+    city: "Rak Maritime City",
+  },
+  {
+    id: 56,
+    city: "Ras al Khaimah",
+  },
+  {
+    id: 57,
+    city: "Ras Al Khor",
+  },
+  {
+    id: 58,
+    city: "Ras Zubbaya (Ras Dubayyah)",
+  },
+  {
+    id: 59,
+    city: "Reem Island",
+  },
+  {
+    id: 60,
+    city: "Ruwais = Ar Ruways",
+  },
+  {
+    id: 61,
+    city: "Ruwais Port",
+  },
+  {
+    id: 62,
+    city: "Sadiyat Island",
+  },
+  {
+    id: 63,
+    city: "Sharjah",
+  },
+  {
+    id: 64,
+    city: "Sila",
+  },
+  {
+    id: 65,
+    city: "Stevin Rock",
+  },
+  {
+    id: 66,
+    city: "Swaihan",
+  },
+  {
+    id: 67,
+    city: "The Palm Jumeirah",
+  },
+  {
+    id: 68,
+    city: "Umm Al Nar",
+  },
+  {
+    id: 69,
+    city: "Umm al Quwain",
+  },
+  {
+    id: 70,
+    city: "Vopak Horizion Terminal",
+  },
+  {
+    id: 71,
+    city: "Yas Island",
+  },
+  {
+    id: 72,
+    city: "Zirku Island",
+  },
+];
+
+export default cities;
