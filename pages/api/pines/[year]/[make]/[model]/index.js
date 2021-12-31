30dbdc3f26730522afc31fe6c79b2c6e3a161707
sheikh-zafar <audios.shkzafar@gmail.com>
2021-12-31 09:15:56 +0530

    commit

--- pages/api/pines/[year]/[make]/[model]/index.js
+++ pages/api/pines/[year]/[make]/[model]/index.js
@@ -2,6 +2,6 @@ import { CARS } from "../../../../../../car-data";
 
 export default function handler(req, res) {
   const { year, make, model } = req.query;
-  const post = CARS.filter((b) => b.year === parseInt(year) && b.make === make && b.model === model);
+  const post = CARS.filter((b) => b.year === parseInt(year) && b.make === make && b.model.replace("%2F","/") === model);
   res.status(200).json(post);
 }
