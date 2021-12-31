42aa02a84eb1c46ac739c9caf742ca9d7023a3e2
sheikh-zafar <audios.shkzafar@gmail.com>
2021-12-31 12:12:16 +0530

    commit

--- pages/api/parts/[partId].js
+++ pages/api/parts/[partId].js
@@ -2,6 +2,6 @@ import { parts } from "../../../parts";
 
 export default function handler(req, res) {
   const { partId } = req.query;
-  const parts = parts.find((a) => a.PARTS === partId);
+  const parts = parts.find((a) => a.parts === partId);
   res.status(200).json(parts);
 }
