3534e69e2401e18d638eb8970bbef6a45a4791d4
sheikh-zafar <audios.shkzafar@gmail.com>
2022-01-08 16:53:17 +0530

    commit

--- Blog.js
+++ Blog.js
@@ -4,7 +4,7 @@ export const BLOG = [
     TITLE: "Ferrari Portofino M",
     CONTENT:
       "<p className=\"text-lg text-gray-800 py-4 sm:mt-5 sm:text-xs md:mt-5 md:text-sm lg:mx-0 lg:text-base xs:text-xs 2xs:text-xs\">The letter m stands for \"modificato\"(meaning nothing but modified version of ferrari). Its just a facelift of original ferrari. Face-lift is a small change in design, updates or improvements made in order to keep a specific car model look fresh and also for boosting up the sales. But ofcourse it's ferrari! it has to be most potential changes in serve function. It is more cosmetic underneath like it has a large air intake in  a bumper. And it has a new little strike connected to air vent. The metal roof from back can be folded in just about 14 seconds to shield the front seat even if you close on the move about 40 km per hour. The superbly pleasant to drive a 20 inch wheel and brembo break calipher. Ferrari portofino does'nt have a muffler which is quite astounding. The ferrari portofino does'nt have a air suspension or nose lift function like other ferrari but they have designed it as a sports car standards.</p><h1 className=\"text-blue-600 text-4xl md:text-lg lg:text-2xl font-extrabold xs:text-base 2xs:text-xs py-5 font-bold\">SPACE AT THE BACK: </h1><p className=\"text-lg text-gray-800 py-4 sm:mt-5 sm:text-xs md:mt-5 md:text-sm lg:mx-0 lg:text-base xs:text-xs 2xs:text-xs\">Even if the car roof is folded away, it has boot capacity about 292 litres.</p><h1 className=\"text-blue-600 text-4xl md:text-lg lg:text-2xl font-extrabold xs:text-base 2xs:text-xs\">REAR: </h1><p className=\"text-lg text-gray-800 py-4 sm:mt-5 sm:text-xs md:mt-5 md:text-sm lg:mx-0 lg:text-base xs:text-xs 2xs:text-xs\">It has 2 little cavities as a rear seats. But its better to use it as space to fit travelling bags or so. Ferrari portofino m comes with a huge rear diffuser which is made possible than the original ferrari version.Petrol particulate filters which makes the exhaust quiet due to which ferrari is able to delete the silencer(muffler).</p><h1 className=\"text-blue-600 text-4xl md:text-lg lg:text-2xl font-extrabold xs:text-base 2xs:text-xs py-5 font-bold\">FRONT SEAT AT THE DRIVER'S SEAT:</h1><p className=\"text-lg text-gray-800 py-4 sm:mt-5 sm:text-xs md:mt-5 md:text-sm lg:mx-0 lg:text-base xs:text-xs 2xs:text-xs\">It is replaced to a touching screen wheareas it would be just a display screen in the previous model. An another amazing feature is that it is inbuilt with a software that consists of feature such as call feature, map direction , music feature and comfort feature like seat adjustment. It also allow us to make our own settings. Exquisite turbine on the front rear drafts the AC vents well. The simple button push which operates the big gearbox. It has got a nice analog tachometer at the front giving a good psycological infuence while driving. The ferrari comes with 5 mode manatina like wet, comfort, speed and esc.</p><h1 className=\"text-blue-600 text-4xl md:text-lg lg:text-2xl font-extrabold xs:text-base 2xs:text-xs py-5 font-bold\">ENGINE:</h1><p className=\"text-lg text-gray-800 py-4 sm:mt-5 sm:text-xs md:mt-5 md:text-sm lg:mx-0 lg:text-base xs:text-xs 2xs:text-xs\">3.9 litres turbo v8 engine petrol, 620 orsepower, 750 newton meter, 8 speed dual clutch gearbox. The front camera in ferrari helps in parking.</p><h1 className=\"text-blue-600 text-4xl md:text-lg lg:text-2xl font-extrabold xs:text-base 2xs:text-xs\">STEERING WHEELS:</h1><p className=\"text-lg text-gray-800 py-4 sm:mt-5 sm:text-xs md:mt-5 md:text-sm lg:mx-0 lg:text-base xs:text-xs xl:text-lg 2xs:text-xs px-5 text-justify\">The carbon fiber steering wheel with LED lights costs 4,300 pounds. Carbon fiber console costs 2,300 pounds and neck warmer costs 2,000 pounds. Carbon cup holder 1,400 pounds rosso portofino paintwork costs etra 7,100 pounds.</p>",
-    IMG: "ferrari_portofino_m",
+    IMG: "ferrari_portofino_m.png",
     AUTHOR: "Emirates_car",
     ALT: "ferrari portofino m",
     TIME: "10 min read",
@@ -12,36 +12,25 @@ export const BLOG = [
   },
   {
     ID: "00002",
-    TITLE: "Ipsum",
+    TITLE: "Ferrari F8 Spider 2021",
     CONTENT:
-      "It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy.",
-    IMG: "https://emirates-car.com/2.png",
-    AUTHOR: "Dummy",
-    ALT: "dummy-variable",
-    TIME: "5 min read",
-    DATE_PUBLISHED: "2 days ago",
+      "<p className=\"text-lg text-gray-800 py-4 sm:mt-5 sm:text-xs md:mt-5 md:text-sm lg:mx-0 lg:text-base xs:text-xs xl:text-lg 2xs:text-xs px-5 text-justify\">F8 Spider is the drop top version of F8 tributo which is the replacement of ferrari 488 gtb. According to ferrari, its slightly less hardcore version of 488 pista. This car was built to celebrate ferrari's V8 Engine. It all started back from 308, 3-2-8, 3-4-8, 3-5-5, 3-6-0, 4-3-0, 4-5-8 and the first turbo charged car came with 4-8-8 and now we have f8. The engine is 8000 rpm turbo charged engine. Th FRONT AIR DUCT at the front which helps all imporatnt downforce on the car when driving. UPWASHING SPOLIER SUCTION to increase the vertical load. It has REAR FLUX DEFLECTORS which reduces dragging. Flat underbody with new FRONT DIFFUSER increased aero efficiency and more efficient brake cooling. It has also got UNDER BODY VORTOX generators to increase vertical load for a good downforce. REAR DIFFUSER with active aerodynamics to maximise performance in both corners and straight lines. NEW FRONT RADIATORS LAYOUT derived from 488 challenge improved cooling and reduced drag 15% air temperature entering the engine to support power increase. F8 spider costs from 2,25,000 pounds. This car has 1,00,000 pounds of optional extras fit to it.</p>",
+    IMG: "ferrari_f8_spider_2021",
+    AUTHOR: "Emirates_car",
+    ALT: "ferrari f8 spider 2021",
+    TIME: "8 min read",
+    DATE_PUBLISHED: "20 days ago",
   },
   {
     ID: "00003",
-    TITLE: "Standard",
+    TITLE: "Ferrari F8 Spider 2022",
     CONTENT:
-      "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.",
-    IMG: "https://emirates-car.com/2.png",
-    AUTHOR: "Dummy",
-    ALT: "dummy-variable",
+      "<p className=\"text-lg text-gray-800 py-4 sm:mt-5 sm:text-xs md:mt-5 md:text-sm lg:mx-0 lg:text-base xs:text-xs xl:text-lg 2xs:text-xs px-5 text-justify\">The price of f8 spider 2022 is $3,50,000 USD. 3.9L twin-turbo v8 engine. 710 HP and 568 LB-FT Torque. 0-6 MPH in 2.9 Seconds. Top speed is 211 MPH. The size of the tire is 305/30 R20. The Ferrari's engine hits a pleasantly sonorous note as it does its work, and the brakes do a masterful job of bringing it to a swift halt. This Ferrari is quick enough to make an indelible impression. The F8 Tributo and Spider are rated at 15 mpg in the city and between 18 and 19 mpg on the highway. The cabin's most notable shortcoming concerns rearward visibility in the coupe. The view out the back is impeded by the rear window's louvers. Many exotics are very low on cargo space, and the F8 is no exception. Cargo capacity stands at seven cubic feet in the coupe.</p>",
+    IMG: "ferrari_f8_spider_2022",
+    AUTHOR: "Emirates_car",
+    ALT: "ferrari f8 spider 2022",
     TIME: "10 min read",
-    DATE_PUBLISHED: "4 days ago",
-  },
-  {
-    ID: "00004",
-    TITLE: "Chunk",
-    CONTENT:
-      'The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.',
-    IMG: "https://emirates-car.com/2.png",
-    AUTHOR: "Dummy",
-    ALT: "dummy-variable",
-    TIME: "3 min read",
-    DATE_PUBLISHED: "11 days ago",
+    DATE_PUBLISHED: "18 days ago",
   },
 ];
 
