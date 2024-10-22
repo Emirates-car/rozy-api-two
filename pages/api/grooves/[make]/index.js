8c98fbfe8ceecd27a2dc6168ff085a314c4411e2
Emirates-car <emiratesautomobileparts@gmail.com>
2024-10-22 06:33:14 -0700

    commit

--- pages/api/grooves/[make]/index.js
+++ pages/api/grooves/[make]/index.js
@@ -1,9 +1,7 @@
-import { CARS } from "../../../../car-data";
+import { CARS } from '../../../../car-data'
 
 export default function handler(req, res) {
-  const { make } = req.query;
-  const post = CARS.filter((b) =>
-    b.make === make
-  );
-  res.status(200).json(post);
+  const { make } = req.query
+  const post = CARS.filter((b) => b.make === make)
+  res.status(200).json(post)
 }
