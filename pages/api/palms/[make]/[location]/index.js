d5a128c21cf47c8a51cf6be3a211e26a8d522d8a
Emirates-car <emiratesautomobileparts@gmail.com>
2024-11-14 20:35:27 -0800

    commit

--- pages/api/palms/[make]/[location]/index.js
+++ pages/api/palms/[make]/[location]/index.js
@@ -2,8 +2,10 @@ import { CARS } from '../../../../../car-data'
 
 export default function handler(req, res) {
   const { make, location } = req.query
+  // Filter to find cars that match both make and location
   const post = CARS.filter(
-    (b) => b.make === make && b.location.filter((c) => c === location)
+    (b) => b.make === make && b.location.includes(location)
   )
+
   res.status(200).json(post)
 }
