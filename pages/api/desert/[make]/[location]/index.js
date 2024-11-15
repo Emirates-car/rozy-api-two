83ef701f978618e59b5a1a227930bae473c238f3
Emirates-car <emiratesautomobileparts@gmail.com>
2024-11-14 20:48:36 -0800

    commit

--- pages/api/desert/[make]/[location]/index.js
+++ pages/api/desert/[make]/[location]/index.js
@@ -6,6 +6,9 @@ export default function handler(req, res) {
   const post = CARS.filter(
     (b) => b.make === make && b.location.includes(location)
   )
+  if (post.length === 0) {
+    return res.status(404).json({ message: 'No matching data found' })
+  }
 
   res.status(200).json(post)
 }
